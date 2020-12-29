4ae5badb7981474c3efe8dd1d8fcc572799bf50e
Szymik <szymonszymiczek32@gmail.com>
2020-12-29 16:10:54 +0100

    constructors

--- src/ApplicationLogic/ActivityFactory.java
+++ src/ApplicationLogic/ActivityFactory.java
@@ -2,7 +2,6 @@ package ApplicationLogic;
 
 public class ActivityFactory {
 
-
     public Activity makeActivity(String activityType, String name, String description, int totalLengthInSec, int valueInClocks, short importance) {
         if (activityType.equals("Periodic")){
             return new PeriodicActivity(valueInClocks, name, description, totalLengthInSec, importance, false, (short) 1);
--- src/ApplicationLogic/OneTimeActivity.java
+++ src/ApplicationLogic/OneTimeActivity.java
@@ -5,5 +5,8 @@ import java.util.Date;
 public class OneTimeActivity extends Activity {
     Date deadline;
 
-
+    public OneTimeActivity(int valueInClocks, String name, String description, int totalLengthInSec, short importance, boolean mustHappen, Date deadline) {
+        super(valueInClocks, name, description, totalLengthInSec, importance, mustHappen);
+        this.deadline = deadline;
+    }
 }
