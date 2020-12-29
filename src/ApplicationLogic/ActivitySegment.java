9c4b5efb696c2a382392d5fcc441c5b1241abf1e
Szymik <szymonszymiczek32@gmail.com>
2020-12-29 23:07:52 +0100

    ActivitySegment getters & setters

--- src/ApplicationLogic/ActivitySegment.java
+++ src/ApplicationLogic/ActivitySegment.java
@@ -1,12 +1,29 @@
 package ApplicationLogic;
 
 public class ActivitySegment {
+
     private int lengthInSec;
     private int valueInClocks;
 
-
     public ActivitySegment(int lengthInSec, int valueInClocks) {
         this.lengthInSec = lengthInSec;
+        this.valueInClocks = valueInClocks;
+    }
+
+    //getters & setters
+    public int getLengthInSec() {
+        return lengthInSec;
+    }
+
+    public void setLengthInSec(int lengthInSec) {
+        this.lengthInSec = lengthInSec;
+    }
+
+    public int getValueInClocks() {
+        return valueInClocks;
+    }
+
+    public void setValueInClocks(int valueInClocks) {
         this.valueInClocks = valueInClocks;
     }
 }
