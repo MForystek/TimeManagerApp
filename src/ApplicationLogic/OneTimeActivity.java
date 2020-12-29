afd90418beda987095eba03e60d230b3c545a5a9
Szymik <szymonszymiczek32@gmail.com>
2020-12-29 13:49:46 +0100

    wprowadzenie zmiennych aktywności

--- src/ApplicationLogic/OneTimeActivity.java
+++ src/ApplicationLogic/OneTimeActivity.java
@@ -1,4 +1,13 @@
 package ApplicationLogic;
 
+import java.util.Date;
+
 public class OneTimeActivity extends Activity {
+    Date deadline;
+
+
+
+
+
+
 }
--- src/ApplicationLogic/ProjectActivity.java
+++ src/ApplicationLogic/ProjectActivity.java
@@ -1,4 +1,8 @@
 package ApplicationLogic;
 
+import java.util.Date;
+
 public class ProjectActivity extends Activity {
+    private short lengthOfSegmentSec;
+    Date deadline;
 }
